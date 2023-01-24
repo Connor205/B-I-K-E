d407ad0ec31a9fb12cbe74264603aa657bc29b1d
Connor Nelson <Conn205@gmail.com>
2023-01-24 17:57:51 -0500

    Python: Initial turret borderplate

---
+++ Python/Turret.py
@@ -0,0 +1,23 @@
+from Arduino import Arduino
+from typing import *
+
+
+class Turret(Arduino):
+
+    def __init__(self, port: str, baud=9600):
+        super().__init__(port, baud)
+
+    def read_handler(self, command: str, value: str):
+        if command == 'STATE':
+            with self.stateLock:
+                self.state = value
+            self.logger.debug("Updated Arduino State to: {}".format(value))
+
+    def turn_to_angle(self, angle: int):
+        self.sendCommand("TURN", values=[angle])
+
+    def shoot_card(self, distance: int):
+        self.sendCommand("FIRE", [distance])
+
+    def return_cards(self):
+        raise NotImplementedError("return_cards is not implemented")
