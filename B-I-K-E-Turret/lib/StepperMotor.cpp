dd2b8314f59c9b7466c4a9a6d51a9844607d8508
Sharwin Patil <sharwin24@gmail.com>
2023-01-27 11:49:42 -0500

    Added degree to steps

--- B-I-K-E-Turret/lib/StepperMotor.cpp
+++ B-I-K-E-Turret/lib/StepperMotor.cpp
@@ -1,2 +1,10 @@
 #include "StepperMotor.hpp"
 #include "TurretConstants.hpp"
+
+
+float StepperMotor::degreeToSteps(float degree) {
+	// Steps per revolution (set on driver)
+	// Steps per revolution (SPR) -> angle (Degrees)
+	// SPR * 1 / 360 = steps per degree * degree
+	return (STEPS_PER_REV / 360.0f) * degree;
+}
