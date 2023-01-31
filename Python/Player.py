add6d284c74088b30cac44d71080210ecb9bf536
TPasserine1287 <passerine.t@northeastern.edu>
2023-01-31 00:14:01 -0500

    Adding Player class and basic methods

---
+++ Python/Player.py
@@ -0,0 +1,36 @@
+from Enums import Seat
+import PlayerHand
+
+class Player():
+    name: str
+    playerID: int
+    seatNumber: Seat
+    stackSize: int
+    inGame: bool
+    hand: PlayerHand
+
+    def __init__(self, name: str, playerID: int) -> None:
+        self.name = name
+        self.playerID = playerID
+        self.seatNumber = None
+        self.stackSize = 0
+        self.inGame = False
+        self.hand = None
+
+    def addToGame(self, seatNumber: Seat) -> None:
+        """Adds a player to a game at the given seat"""
+        self.seatNumber = seatNumber
+        self.inGame = True
+
+    def removeFromGame(self) -> None:
+        """Removes the player from the game"""
+        self.seatNumber = None
+        self.inGame = False
+
+    def makeBet(self, betSize: int) -> None:
+        """Changing stack size based off of this player's bet"""
+        self.stackSize -= betSize
+
+    def winPot(self, potSize:int) -> None:
+        """Changing stack size based off of the player's win"""
+        self.stackSize += potSize
