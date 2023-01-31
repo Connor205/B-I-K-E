cede58e94115af94b07d61fca150521af5d6e5af
TPasserine1287 <passerine.t@northeastern.edu>
2023-01-30 23:11:17 -0500

    Adding Deck class and basic methods

---
+++ Python/Deck.py
@@ -0,0 +1,16 @@
+from Card import Card
+
+class Deck():
+    """Class representing a Deck of Cards in poker"""
+    cards: Card = []
+
+    def __init__(self) -> None:
+        """Initializes an unshuffled deck of 52 cards"""
+        for suit in suit:
+            for value in value:
+                newCard = Card(value, suit)
+                self.cards.append(newCard)
+    
+    def shuffleDeck(self):
+        """Shuffles this deck of cards"""
+        self.cards.shuffle()
