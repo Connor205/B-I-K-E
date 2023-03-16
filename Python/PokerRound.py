db8df2c0e72a9d1ec8a5300f1953ce78cc86039d
TPasserine1287 <passerine.t@northeastern.edu>
2023-03-16 16:08:58 -0400

    Implementing most remaining method stubs

--- Python/PokerRound.py
+++ Python/PokerRound.py
@@ -7,10 +7,9 @@ from Player import Player
 class PokerRound():
     potSize: int
     state: GameState
-    hands: list[PlayerHand]
     communityCards: list[Card]
     deck: Deck
-    playerIDs: list[int]
+    players: list[Player]
     turnIndex: int # keeps track of who's turn it is to bet
     currentPlayer: Player # keeps track of who's turn it is to bet
     betToMatch: int # the current bet to match
@@ -18,8 +17,22 @@ class PokerRound():
     def __init__(self) -> None:
         self.playerIDs = []
         self.potSize = 0
-        # self.state = PREPARING
-        self.hands = []
+        self.state = GameState.PREPARING
         self.communityCards = []
         self.deck = Deck()
         self.turnIndex = 0
+        self.betToMatch = 0
+
+    def playerFolds(self) -> bool:
+        origSize = len(self.players)
+        self.players.remove(self.currentPlayer)
+        return len(self.players) == origSize - 1
+    
+    def startRound(self) -> bool:
+        allReady = True
+        for player in self.players:
+            allReady = allReady and player.isReady
+        if allReady:
+            self.state = GameState.PREFLOP
+            self.currentPlayer = self.players[self.turnIndex]
+
