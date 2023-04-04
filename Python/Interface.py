67a7343e85336271380585284596f6a30f26d88c
Connor Nelson <Conn205@gmail.com>
2023-04-04 18:37:24 -0400

    Feat: adding python side of interface

--- Python/Interface.py
+++ Python/Interface.py
@@ -20,6 +20,11 @@ class Interface(Arduino):
             self.logger.error("Unknown Command: {}".format(command))
             return
 
+        # Panels are labeled 0-3 from left to right while facing the table (and screen)
+        # 0 is the one closest to the shuffler, 3 is the one closest to the estop
+        # Buttons are labels right to left (0 -> 7) for each panel
+        # From 0->7 we have: Black, Red, Blue, White, Fold, Bet, Check, Call
+
         # Value is panel, buttonIndex
         panel, buttonIndex = value.split(',')
         self.logger.debug("Panel: {} | Button: {}".format(panel, buttonIndex))
