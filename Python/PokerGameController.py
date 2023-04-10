5a1cf0a14206b4e4eeffc36839822a0a123f3c38
Chris Swagler <76789839+chrisswagler@users.noreply.github.com>
2023-04-10 18:09:21 -0400

    Add popups before blocking calls and for winning; update handranking to str

--- Python/PokerGameController.py
+++ Python/PokerGameController.py
@@ -120,10 +120,16 @@ class PokerGameController():
                     # Remove the ready status text from all players in view
                     self.view.clearReadyStatusText()
 
-                    # TODO: Update the view to say "Please put cards in and press deal confirm"
+                    # Update the view to say "Please put cards in and press deal confirm"
+                    self.view.createPopup("Please put the cards in the turret", "Press the dealer confirmation button to continue")
+                    # The turret call is blocking, so let's update the view before calling it
+                    self.view.update()
 
                     # Call the blocking method for the turret, waiting for confirmation button
                     self.turret.waitForConfirmation()
+
+                    # Once confirmed, we can clear the popup and start the round
+                    self.view.clearPopups()
 
                     # If in state preparing, we just advance the round to start it
                     self.model.advanceRound()
@@ -297,14 +303,25 @@ class PokerGameController():
                 self.view.dealRiver()
                 self.view.indicatePlayerTurn(self.model.currentRound.currentPlayer.seatNumber)
             case GameState.SHOWDOWN:
-                # TODO: Have view display winner, wait for button confirmation from winner to end hand
-                for player in self.model.currentRound.determineWinners():
-                    self.view.indicatePlayerTurn(player.seatNumber)
-                pass
+                # Have view display winner, wait for button confirmation from winner to end hand
+                winners = self.model.currentRound.determineWinners()
+                if len(winners) == 1:
+                    self.view.createPopup(winners[0].name + " wins with " + str(winners[0].getHand().getRanking()) + " and gets " + str(self.model.getPotSize()) + " chips!", "Press any button to continue")
+                else:
+                    # Print out all the winners with their winning and the overall pot size
+                    winnerString = ""
+                    for winner in winners:
+                        winnerString += winner.name + " with " + str(winner.getHand().getRanking()) + ", "
+                    winnerString = winnerString[:-2]
+                    self.view.createPopup(winnerString + " split the pot of " + str(self.model.getPotSize()) + " chips!", "Press any button to continue")
             case GameState.POSTHAND:
                 remainingCards = self.model.getRemainingCards()
                 self.turret.dealDiscard(remainingCards)
+                self.view.createPopup("Please put the cards in the shuffler", "Press the shuffler confirmation button to continue")
+                # Turret waits for confirmation from the shuffler and is blocking so update the view
+                self.view.update()
                 self.turret.waitForConfirmation()
+                self.view.clearPopups()
 
     def updateViewText(self) -> None:
         # Update the pot size in the view based on the model
